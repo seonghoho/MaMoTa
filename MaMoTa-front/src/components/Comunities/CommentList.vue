0ff7b56bc660e67658275b7c96c47eaa5b69382a
GISUNOH <pakox123@gmail.com>
2023-11-20 17:29:33 +0900

    댓글추가

--- MaMoTa-front/src/components/Comunities/CommentList.vue
+++ MaMoTa-front/src/components/Comunities/CommentList.vue
@@ -3,14 +3,34 @@
 <template>
   <li>
     <h3>야 댓글좀</h3>
-    <!-- {{ comment }} -->
+    <!-- {{ comment }}댓글 개체 확인 -->
+    <!-- <p>{{ comment.article }}</p> -->
 
     <span class="comment-id">{{ comment.id }} {{ comment.username }}</span>
     <span class="comment-content">{{ comment.content }}</span>
+
+    
+    <!--댓글 삭제부분<button 
+      v-if="comment.user.username === authStore.user.username"
+      @click="commentDelete"
+    >
+      🗑
+    </button> -->
   </li>
 </template>
 
 <script setup>
+// import { useCommentStore } from '@/stores/comments';
+// import { useAuthStore } from '@/stores/auth';
+// const authStore = useAuthStore()
+// const store = useCommentStore()
+
+// const commentDelete = () => {
+//   store.commentDelete(comment.article, comment.id)
+// }
+
+
+
 defineProps({
   comment: Object
 })
