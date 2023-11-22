05858f4b48b5f23f605845af4e0ca0be69666fb3
SeonWook Gim <tjsdnr960@naver.com>
2023-11-22 17:42:06 +0900

    CSS 다양한 수정

--- MaMoTa-front/src/App.vue
+++ MaMoTa-front/src/App.vue
@@ -96,7 +96,5 @@ h2 {
 .btn-close {
   background-color: rgb(233, 42, 233);
 }
-@font-face{
-font-family:'DNFBitBitv2';
-font-style:normal;font-weight:400;src:url('//cdn.df.nexon.com/img/common/font/DNFBitBitv2.otf')format('opentype')}
+
 </style>
